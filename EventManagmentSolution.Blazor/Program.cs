8ccf7c3b6e1f447ee9aab5aacfec021c2cc29bc7
Behnam <behnam.vosoogh@outlook.com>
2024-07-13 01:26:48 -0400

    client side added

---
+++ EventManagmentSolution.Blazor/Program.cs
@@ -0,0 +1,24 @@
+using EventManagmentSolution.Blazor;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddMsalAuthentication(options =>
+{
+    builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+    options.ProviderOptions.DefaultAccessTokenScopes.Add("[YOUR_URI");
+});
+
+builder.Services.AddOidcAuthentication(options =>
+{
+    // Configure your authentication provider options here.
+    // For more information, see https://aka.ms/blazor-standalone-auth
+    builder.Configuration.Bind("Local", options.ProviderOptions);
+});
+
+await builder.Build().RunAsync();
