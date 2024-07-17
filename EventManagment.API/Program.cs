96c6e428de29ed9030524faf89fd6b6c688d701a
Behnam <behnam.vosoogh@outlook.com>
2024-07-17 15:51:43 -0400

    serive extension used

--- EventManagment.API/Program.cs
+++ EventManagment.API/Program.cs
@@ -1,51 +1,39 @@
+using EventManagment.API.Extensions;
 using EventManagment.Models.Data;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Identity.Web;
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
-
-
-
-// Add services to the container.
-builder.Services.AddCors(options =>
+internal class Program
 {
-    options.AddPolicy("CorsPolicy", policy =>
+    private static void Main(string[] args)
     {
-        policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
-    });
-});
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),sqlOptions =>
-    {
-        sqlOptions.MigrationsAssembly("EventManagment.API");
-    });
-});
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddB2CAuthentication(builder.Configuration);
+        builder.Services.AddApplicationDatabaseContext(builder.Configuration);
+        builder.Services.ConfigureCors();
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+        var app = builder.Build();
 
-var app = builder.Build();
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+        app.UseHttpsRedirection();
+        app.UseCors("CorsPolicy");
+        app.UseAuthentication();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseCors("CorsPolicy");
-app.UseAuthentication();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
