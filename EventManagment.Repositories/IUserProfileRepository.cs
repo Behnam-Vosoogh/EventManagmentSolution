64cf42cdbaab7659b6ba1166489a9f1b59e3050d
Behnam <behnam.vosoogh@outlook.com>
2024-07-17 16:05:31 -0400

    IUserProfileRepository created

---
+++ EventManagment.Repositories/IUserProfileRepository.cs
@@ -0,0 +1,22 @@
+﻿using EventManagment.Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventManagment.Repositories
+{
+    public  interface IUserProfileRepository
+    {
+        Task CreateAsync(UserProfile userProfile);
+
+        Task<UserProfile> GetByIdAsync(string id);
+
+        Task<UserProfile> GetByUserId(string userId);
+
+        void Remove(UserProfile userProfile);
+
+        void Update(UserProfile userProfile);
+    }
+}
