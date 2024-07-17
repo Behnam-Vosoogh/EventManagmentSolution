2c859791d2df668acc9cd24286dbaab7d6b51616
Behnam <behnam.vosoogh@outlook.com>
2024-07-17 15:50:40 -0400

    service extension created

---
+++ EventManagment.API/Extensions/ServiceExtensions.cs
@@ -0,0 +1,36 @@
+﻿using EventManagment.Models.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Web;
+
+namespace EventManagment.API.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void AddB2CAuthentication(this IServiceCollection services , IConfiguration configuration) 
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddMicrosoftIdentityWebApi(configuration.GetSection("AzureAd"));
+        }
+        public static void AddApplicationDatabaseContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"), sqlOptions =>
+                {
+                    sqlOptions.MigrationsAssembly("EventManagment.API");
+                });
+            });
+        }
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+                });
+            });
+        }
+    }
+}
